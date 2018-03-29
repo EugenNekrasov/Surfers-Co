0495087472a889b7d9329dcc09105d508dd90193
gendalf7771 <e.nekrasov91@gmail.com>
2018-03-29 17:51:16 +0300

    The project structure has been reorganized in order to easy deploy

--- src/js/script.js
+++
@@ -1,6 +0,0 @@
-let burger = document.getElementsByClassName("burger")[0];
-let headerNav = document.getElementsByClassName("header__nav")[0];
-
-burger.onclick = function(){
-	headerNav.style.display = (headerNav.style.display == "block") ? "none" : "block";
-};
